79ca37750345c34280ae9cdebac17b994ca2a4f8
Mukhammadali <mukhammadalime@gmail.com>
2022-05-13 07:26:41 +0900

    Added an alert for successful bookings

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -55,7 +55,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
-  const user = (await User.findOne({ email: session.customer_email })).id;
+  const user = (await User.findOne({ email: session.customer_email }))._id;
   const price = session.amount_total / 100;
 
   await Booking.create({ tour, user, price });
