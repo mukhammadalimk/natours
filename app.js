ea449c6090a803460cf590743dbe6041b9c93112
Mukhammadali <mukhammadalime@gmail.com>
2022-05-13 04:38:11 +0900

    Added cors

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 // const bodyParser = require('body-parser');
 const compression = require('compression');
-// const cors = require('cors');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -29,14 +29,14 @@ app.set('views', path.join(__dirname, 'views'));
 
 // 1) GLOBAL MIDDLEWARES
 // Implement cors
-// app.use(cors());
+app.use(cors());
 // Access-Control-Allow-Origin *
 // api.natours.com, front-end natours.com
 // app.use(cors({
 //   origin: 'https://www.natours.com'
 // }))
 
-// app.options('*', cors());
+app.options('*', cors());
 // app.options('/api/v1/tours/:id', cors());
 
 // Serving static files
